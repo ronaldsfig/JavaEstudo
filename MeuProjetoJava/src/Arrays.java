9a70af34e733427b92b8b9f62ba7a47436c564c8
Ronaldo Figueiredo <73131760+ronaldsfig@users.noreply.github.com>
2020-11-22 14:51:43 -0300

    Update Arrays.java

--- MeuProjetoJava/src/Arrays.java
+++ MeuProjetoJava/src/Arrays.java
@@ -1,6 +1,24 @@
 public class Arrays {
     public static void main(String[] args) {
-        int[] idades;
-        
+        String[] nomes = new String[3];
+        nomes[0] = "Ronaldo";
+        nomes[1] = "Maria";
+        nomes[2] = "Pedro";
+
+        for (String s : nomes) {
+            System.out.println(s);
+        }
+
+        int[] numeros = {20, 15, 62};
+
+        for (int i : numeros) {
+            System.out.println(i);
+        }
+
+        boolean[] status = {true, false, true};
+
+        for (boolean b : status) {
+            System.out.println(b);
+        }
     }
 }
