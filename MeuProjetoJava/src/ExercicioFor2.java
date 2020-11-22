28cbe604020beafddb71ce431a11ec5512145ad7
Ronaldo Figueiredo <73131760+ronaldsfig@users.noreply.github.com>
2020-11-22 12:16:16 -0300

    update 22/11

--- MeuProjetoJava/src/ExercicioFor2.java
+++ MeuProjetoJava/src/ExercicioFor2.java
@@ -1,11 +1,12 @@
 public class ExercicioFor2 {
     public static void main(String[] args) {
-        double valorCarro = 30000;
+        double valorCarro = 15000;
         for (int parcelas = 0; parcelas < valorCarro; parcelas++) {
             double valorParcela = valorCarro / parcelas;
-            if (valorParcela >= 1000) {
-                System.out.println("Parcela "+parcelas+" R$ "+valorParcela);
-            };
+            if (valorParcela < 1000) {
+                continue;
+            }
+            System.out.println("Parcela "+parcelas+" R$ "+valorParcela);
         };
     }
 }
